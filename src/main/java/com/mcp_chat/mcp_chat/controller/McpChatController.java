4dee53bad26265a291603a4091cbd96b5b4a8c80
전병일 <jeonbye123@gmail.com>
2025-05-25 18:02:57 +0900

    message객체 활용

--- src/main/java/com/mcp_chat/mcp_chat/controller/McpChatController.java
+++ src/main/java/com/mcp_chat/mcp_chat/controller/McpChatController.java
@@ -4,6 +4,7 @@ import com.mcp_chat.mcp_chat.mcp_chat.McpChatClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.model.Generation;
@@ -18,6 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import reactor.core.publisher.Flux;
 
+import javax.validation.constraints.NotNull;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -27,20 +30,31 @@ public class McpChatController {
     private final McpChatClient chatModel;
 
     @GetMapping("/ai/generate")
-    public Map<String,String> generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+    public Map<String,String> generate(@RequestParam(value = "message") @NotNull String message) {
         ChatClient chatClient = chatModel.getClient();
 
         return Map.of("generation", chatClient
                 .prompt(message)
                 .call()
                 .chatResponse()
+                .getResult()
                 .toString());
     }
 
     @GetMapping("/ai/generate/custom")
     public Map<String, Object> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
-        PromptTemplate template = new PromptTemplate("안녕하세요 저의 이름은 {name}입니다. 직업은 {job}이구.");
-        Prompt prompt = template.create(Map.of("name", "전병일", "job", "개발자"));
+        //1.기본 메시지 처라
+        /*String format = "안녕하세요 저의 이름은 {name}입니다.";
+        PromptTemplate template = new PromptTemplate(Message);
+         Prompt prompt = template.create(Map.of("name", message));
+        */
+
+        //2. Message객체를 이용한 더욱 유연한 응답처리
+        SystemMessage systemMessage = new SystemMessage("이름이과 직업에 관련 없는 요청이 들어오면 '이름을 말씀해주세요 라고 알려줘'");
+        UserMessage userMessage = new UserMessage(String.format("안녕하세요 저의 이름은 {name}입니다.", message));
+        Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
+
+
         Map<String, Object> context = this.chatModel.getClient().prompt(prompt).call().chatClientResponse().context();
 
         return context;
